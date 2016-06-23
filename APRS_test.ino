102298bc6b2697c7058aa483d8b8f227085c305e
Zachary Burnett <zrb@umd.edu>
2016-06-23 15:19:58 -0400

    this is NOT Python

--- APRS_test.ino
+++ APRS_test.ino
@@ -1,10 +1,11 @@
 #include "Arduino.h"
 
 String test_string =
-        "W3EAX-12>APRS,N3KTX-4*,WIDE1*,WIDE2-1,qAR,W4TTU:!/:IgX:iOnO   /A=000633|!#|  /W3EAX,19,8,16'C,http://www.umd.edu"
+        "W3EAX-12>APRS,N3KTX-4*,WIDE1*,WIDE2-1,qAR,W4TTU:!/:IgX:iOnO   /A=000633|!#|  /W3EAX,19,8,16'C,http://www.umd.edu";
 
 void setup()
 {
+    Serial.begin(9600);
 }
 
 void loop()
