41e58772caf77418b16d768291e9289d932bc6a0
Zachary Burnett <zrb@umd.edu>
2016-06-23 15:32:19 -0400

    i'm dumb

--- APRS_test.ino
+++ APRS_test.ino
@@ -1,7 +1,13 @@
 #include "Arduino.h"
 
-String test_string =
-        "W3EAX-12>APRS,N3KTX-4*,WIDE1*,WIDE2-1,qAR,W4TTU:!/:IgX:iOnO   /A=000633|!#|  /W3EAX,19,8,16'C,http://www.umd.edu";
+String test_strings[] =
+        {
+                "W3EAX-9>APRS,N3KTX-4*,WIDE1*,WIDE2-1,qAR,W4TTU:!/:IgL:iOrO   /A=000541|!#|  /W3EAX,18,8,15'C,http://www.umd.edu",
+                "W3EAX-9>APRS,N3KTX-4*,WIDE1*,WIDE2-1,qAR,W4TTU:!/:Ig`:iOhO   /A=000557|!$|  /W3EAX,23,8,16'C,http://www.umd.edu",
+                "W3EAX-9>APRS,N3KTX-4*,WIDE1*,WIDE2*,qAR,N3ENM-10:!/:Ig`:iOhO   /A=000557|!$|  /W3EAX,23,8,16'C,http://www.umd.edu",
+                "W3EAX-9>APRS,N3KTX-4*,WIDE1*,WIDE2-1,qAR,W4TTU:!/:IgV:iOhO   /A=000613|!'|  /W3EAX,39,8,19'C,http://www.umd.edu" };
+
+int num_strings = 4;
 
 void setup()
 {
@@ -10,6 +16,9 @@ void setup()
 
 void loop()
 {
-    Serial.println(test_string);
-    delay(250);
+    for (int index = 0; index < num_strings; index++)
+    {
+        Serial.println(test_strings[index]);
+        delay(250);
+    }
 }
